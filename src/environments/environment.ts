06e8bb68848d02be112d65fc87fe4beabefb253f
jdpo2008 <jdpo2008@gmail.com>
2020-11-24 01:25:52 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyDACAu8iXFMFg-Kq_cKa__m_TtawZctFRM",
-    authDomain: "angular-app-ac31d.firebaseapp.com",
-    databaseURL: "https://angular-app-ac31d.firebaseio.com",
-    projectId: "angular-app-ac31d",
-    storageBucket: "angular-app-ac31d.appspot.com",
-    messagingSenderId: "525239353976",
-    appId: "1:525239353976:web:874508e8f688242569638d",
-    measurementId: "G-4CC0E4RGLJ",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "d",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
   },
 };
 
